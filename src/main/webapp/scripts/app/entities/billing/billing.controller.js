8c8d983dc2e65ffa3a023c78b640d36532dd68e4
prashanth.mergu <prashanth.mergu@xeon.co.za>
2016-10-21 00:32:04 +0530

    removed method not supported exception

--- src/main/webapp/scripts/app/entities/billing/billing.controller.js
+++ src/main/webapp/scripts/app/entities/billing/billing.controller.js
@@ -7,21 +7,14 @@ angular.module('portalApp')
         $scope.predicate = 'id';
         $scope.reverse = true;
         $scope.page = 1;
-        $scope.loadAll = function() {
-        	Billing.query({page: $scope.page - 1, size: 1000, sort: [$scope.predicate + ',' + ($scope.reverse ? 'asc' : 'desc'), 'id']}, function(result, headers) {
-                $scope.links = ParseLinks.parse(headers('link'));
-                $scope.totalItems = headers('X-Total-Count');
-                $scope.billings = result;
-            });
-        };
+        
         $scope.loadPage = function(page) {
             $scope.page = page;
-            $scope.loadAll();
         };
-        $scope.loadAll();
+         
 
         $scope.refresh = function () {
-            $scope.loadAll();
+            
             $scope.clear();
         };
 
