61f46792d5bffdc6630829e742a953379387c995
prashanth.mergu <prashanth.mergu@xeon.co.za>
2016-10-27 00:56:57 +0530

    update

--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -120,4 +120,53 @@ angular.module('portalApp', ['LocalStorageModule',
             is: function(val) { return [true,false,0,1].indexOf(val) >= 0; },
             pattern: /bool|true|0|1/
         });
-    }]);
+    }]).directive('validNumber', function() {
+        return {
+            require: '?ngModel',
+            link: function(scope, element, attrs, ngModelCtrl) {
+            	
+            	
+              if(!ngModelCtrl) {
+                return; 
+              }
+
+              ngModelCtrl.$parsers.push(function(val) {
+                if (angular.isUndefined(val)) {
+                    var val = '';
+                }
+                
+                 
+                val=val+'';
+                var clean = val.replace(/[^-0-9\.]/g, '');
+                var negativeCheck = clean.split('-');
+      			var decimalCheck = clean.split('.');
+                if(!angular.isUndefined(negativeCheck[1])) {
+                    negativeCheck[1] = negativeCheck[1].slice(0, negativeCheck[1].length);
+                    clean =negativeCheck[0] + '-' + negativeCheck[1];
+                    if(negativeCheck[0].length > 0) {
+                    	clean =negativeCheck[0];
+                    }
+                    
+                }
+                  
+                if(!angular.isUndefined(decimalCheck[1])) {
+                    decimalCheck[1] = decimalCheck[1].slice(0,3);
+                    clean =decimalCheck[0] + '.' + decimalCheck[1];
+                }
+
+                if (val !== clean) {
+                  ngModelCtrl.$setViewValue(clean);
+                  ngModelCtrl.$render();
+                }
+                return clean;
+              });
+
+              element.bind('keypress', function(event) {
+                if(event.keyCode === 32) {
+                  event.preventDefault();
+                }
+              });
+            }
+          };
+        });;
+
