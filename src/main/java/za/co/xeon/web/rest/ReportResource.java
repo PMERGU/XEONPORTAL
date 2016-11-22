fb0c6982af7c686693025eab7eb738e4b3b23517
prashanth.mergu <prashanth.mergu@xeon.co.za>
2016-11-22 20:18:41 +0530

    update PDF Report

--- src/main/java/za/co/xeon/web/rest/ReportResource.java
+++ src/main/java/za/co/xeon/web/rest/ReportResource.java
@@ -31,6 +31,8 @@ import com.codahale.metrics.annotation.Timed;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Element;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
@@ -46,7 +48,6 @@ import za.co.xeon.repository.AttachmentRepository;
 import za.co.xeon.repository.PurchaseOrderRepository;
 import za.co.xeon.repository.UserRepository;
 import za.co.xeon.web.rest.dto.PODReportReqDTO;
-import za.co.xeon.web.rest.dto.PODReportResDTO;
 import za.co.xeon.web.rest.dto.StockReportDTO;
 import za.co.xeon.web.rest.util.PaginationUtil;
 
@@ -139,73 +140,73 @@ public class ReportResource {
 	}
 
 	public ByteArrayOutputStream createPdf(List<StockData> ret) {
-		Document document = new Document();
+		Document document = new Document(PageSize.A3);
 		try {
 			// PdfWriter writer = PdfWriter.getInstance(document, new
 			// FileOutputStream("AddTableExample.pdf"));
+			Float fntSize=6.7f;
 			ByteArrayOutputStream out = new ByteArrayOutputStream();
 			PdfWriter writer = PdfWriter.getInstance(document, out);
 			document.open();
-
 			PdfPTable table = new PdfPTable(10); // 3 columns.
 			table.setWidthPercentage(100); // Width 100%
 			table.setSpacingBefore(10f); // Space before table
 			table.setSpacingAfter(10f); // Space after table
 
 			// Set Column widths
-			float[] columnWidths = { 0.5f, 0.5f, 1f, 1f, 1f, 1f, 0.5f,1f, 0.5f, 1f };
+			float[] columnWidths = { 0.3f, 0.5f, 1f, 2f, 1f, 0.5f, 0.5f,1f, 0.5f, 0.5f };
 			table.setWidths(columnWidths);
 
-			PdfPCell iDHeader = new PdfPCell(new Paragraph("ID"));
+			PdfPCell iDHeader = new PdfPCell(new Paragraph("ID" , FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 			iDHeader.setBorderColor(BaseColor.BLACK);
 			iDHeader.setPaddingLeft(10);
 			iDHeader.setHorizontalAlignment(Element.ALIGN_CENTER);
 			iDHeader.setVerticalAlignment(Element.ALIGN_MIDDLE); 
 			table.addCell(iDHeader);
 			
-			PdfPCell wnHeader = new PdfPCell(new Paragraph("Warehouse Number"));
+			PdfPCell wnHeader = new PdfPCell(new Paragraph("Warehouse Number", FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 			wnHeader.setBorderColor(BaseColor.BLACK);
 			wnHeader.setPaddingLeft(10);
 			wnHeader.setHorizontalAlignment(Element.ALIGN_CENTER);
 			wnHeader.setVerticalAlignment(Element.ALIGN_MIDDLE);
 			table.addCell(wnHeader);
 			
-			PdfPCell mtHeader = new PdfPCell(new Paragraph("Material"));
+			PdfPCell mtHeader = new PdfPCell(new Paragraph("Material", FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 			mtHeader.setBorderColor(BaseColor.BLACK);
 			mtHeader.setPaddingLeft(10);
 			mtHeader.setHorizontalAlignment(Element.ALIGN_CENTER);
 			mtHeader.setVerticalAlignment(Element.ALIGN_MIDDLE);
 			table.addCell(mtHeader);
 			
-			PdfPCell mtDsHeader = new PdfPCell(new Paragraph("Material Description"));
+			PdfPCell mtDsHeader = new PdfPCell(new Paragraph("Material Description", FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 			mtDsHeader.setBorderColor(BaseColor.BLACK);
 			mtDsHeader.setPaddingLeft(10);
 			mtDsHeader.setHorizontalAlignment(Element.ALIGN_CENTER);
 			mtDsHeader.setVerticalAlignment(Element.ALIGN_MIDDLE);
 			table.addCell(mtDsHeader);
 			
-			PdfPCell stHeader = new PdfPCell(new Paragraph("Storage Type"));
+			PdfPCell stHeader = new PdfPCell(new Paragraph("Storage Type", FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 			stHeader.setBorderColor(BaseColor.BLACK);
 			stHeader.setPaddingLeft(10);
 			stHeader.setHorizontalAlignment(Element.ALIGN_CENTER);
 			stHeader.setVerticalAlignment(Element.ALIGN_MIDDLE);
 			table.addCell(stHeader);
 			
-			PdfPCell batchCell = new PdfPCell(new Paragraph("Batch "));
+			PdfPCell batchCell = new PdfPCell(new Paragraph("Batch ", FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 			batchCell.setBorderColor(BaseColor.BLACK);
 			batchCell.setPaddingLeft(10);
 			batchCell.setHorizontalAlignment(Element.ALIGN_CENTER);
 			batchCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
 			table.addCell(batchCell);
 			
-			PdfPCell putAwayQnant = new PdfPCell(new Paragraph("Put ways"));
+			PdfPCell putAwayQnant = new PdfPCell(new Paragraph("Put ways", FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 			putAwayQnant.setBorderColor(BaseColor.BLACK);
 			putAwayQnant.setPaddingLeft(10);
 			putAwayQnant.setHorizontalAlignment(Element.ALIGN_CENTER);
 			putAwayQnant.setVerticalAlignment(Element.ALIGN_MIDDLE);
 			table.addCell(putAwayQnant);
 			
-			PdfPCell availableQunatity = new PdfPCell(new Paragraph("Available"));
+			PdfPCell availableQunatity = new PdfPCell(new Paragraph("Available", FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 			availableQunatity.setBorderColor(BaseColor.BLACK);
 			availableQunatity.setPaddingLeft(10);
 			availableQunatity.setHorizontalAlignment(Element.ALIGN_CENTER);
@@ -213,14 +214,14 @@ public class ReportResource {
 			table.addCell(availableQunatity);
 			
 
-			PdfPCell pickQuant = new PdfPCell(new Paragraph("Picks"));
+			PdfPCell pickQuant = new PdfPCell(new Paragraph("Picks", FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 			pickQuant.setBorderColor(BaseColor.BLACK);
 			pickQuant.setPaddingLeft(10);
 			pickQuant.setHorizontalAlignment(Element.ALIGN_CENTER);
 			pickQuant.setVerticalAlignment(Element.ALIGN_MIDDLE);
 			table.addCell(pickQuant);
 			
-			PdfPCell uomHeader = new PdfPCell(new Paragraph("UoM"));
+			PdfPCell uomHeader = new PdfPCell(new Paragraph("UoM", FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 			uomHeader.setBorderColor(BaseColor.BLACK);
 			uomHeader.setPaddingLeft(10);
 			uomHeader.setHorizontalAlignment(Element.ALIGN_CENTER);
@@ -232,56 +233,56 @@ public class ReportResource {
 			if (ret != null && ret.size() > 0) {
 				int i = 1;
 				for (StockData data : ret) {
-					PdfPCell iDdata = new PdfPCell(new Paragraph(i + ""));
+					PdfPCell iDdata = new PdfPCell(new Paragraph(i + "", FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 					iDdata.setBorderColor(BaseColor.BLACK);
 					iDdata.setPaddingLeft(10);
 					iDdata.setHorizontalAlignment(Element.ALIGN_CENTER);
 					iDdata.setVerticalAlignment(Element.ALIGN_MIDDLE);
 					table.addCell(iDdata);
 					 
-					PdfPCell wnData = new PdfPCell(new Paragraph(data.get_lgnum()));
+					PdfPCell wnData = new PdfPCell(new Paragraph(data.get_lgnum(), FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 					wnData.setBorderColor(BaseColor.BLACK);
 					wnData.setPaddingLeft(10);
 					wnData.setHorizontalAlignment(Element.ALIGN_CENTER);
 					wnData.setVerticalAlignment(Element.ALIGN_MIDDLE);
 					table.addCell(wnData);
 					
-					PdfPCell mtData = new PdfPCell(new Paragraph(data.get_matnr()));
+					PdfPCell mtData = new PdfPCell(new Paragraph(data.get_matnr(), FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 					mtData.setBorderColor(BaseColor.BLACK);
 					mtData.setPaddingLeft(10);
 					mtData.setHorizontalAlignment(Element.ALIGN_CENTER);
 					mtData.setVerticalAlignment(Element.ALIGN_MIDDLE);
 					table.addCell(mtData);
 					
-					PdfPCell mtDsData = new PdfPCell(new Paragraph(data.get_maktx()));
+					PdfPCell mtDsData = new PdfPCell(new Paragraph(data.get_maktx(), FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 					mtDsData.setBorderColor(BaseColor.BLACK);
 					mtDsData.setPaddingLeft(10);
 					mtDsData.setHorizontalAlignment(Element.ALIGN_CENTER);
 					mtDsData.setVerticalAlignment(Element.ALIGN_MIDDLE);
 					table.addCell(mtDsData);
 					
-					PdfPCell stData = new PdfPCell(new Paragraph(data.get_lgtyp()));
+					PdfPCell stData = new PdfPCell(new Paragraph(data.get_lgtyp(), FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 					stData.setBorderColor(BaseColor.BLACK);
 					stData.setPaddingLeft(10);
 					stData.setHorizontalAlignment(Element.ALIGN_CENTER);
 					stData.setVerticalAlignment(Element.ALIGN_MIDDLE);
 					table.addCell(stData);
 					
-					PdfPCell batchCellData = new PdfPCell(new Paragraph(data.get_charg()));
+					PdfPCell batchCellData = new PdfPCell(new Paragraph(data.get_charg(), FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 					batchCellData.setBorderColor(BaseColor.BLACK);
 					batchCellData.setPaddingLeft(10);
 					batchCellData.setHorizontalAlignment(Element.ALIGN_CENTER);
 					batchCellData.setVerticalAlignment(Element.ALIGN_MIDDLE);
 					table.addCell(batchCellData);
 					
-					PdfPCell putAwayQnantData = new PdfPCell(new Paragraph(data.get_einme()+""));
+					PdfPCell putAwayQnantData = new PdfPCell(new Paragraph(data.get_einme()+"", FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 					putAwayQnantData.setBorderColor(BaseColor.BLACK);
 					putAwayQnantData.setPaddingLeft(10);
 					putAwayQnantData.setHorizontalAlignment(Element.ALIGN_CENTER);
 					putAwayQnantData.setVerticalAlignment(Element.ALIGN_MIDDLE);
 					table.addCell(putAwayQnantData);
 					
-					PdfPCell availableQunatityData = new PdfPCell(new Paragraph(data.get_verme()+""));
+					PdfPCell availableQunatityData = new PdfPCell(new Paragraph(data.get_verme()+"", FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 					availableQunatityData.setBorderColor(BaseColor.BLACK);
 					availableQunatityData.setPaddingLeft(10);
 					availableQunatityData.setHorizontalAlignment(Element.ALIGN_CENTER);
@@ -289,14 +290,14 @@ public class ReportResource {
 					table.addCell(availableQunatityData);
 					
 
-					PdfPCell pickQuantData = new PdfPCell(new Paragraph(data.get_einme()+""));
+					PdfPCell pickQuantData = new PdfPCell(new Paragraph(data.get_einme()+"", FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 					pickQuantData.setBorderColor(BaseColor.BLACK);
 					pickQuantData.setPaddingLeft(10);
 					pickQuantData.setHorizontalAlignment(Element.ALIGN_CENTER);
 					pickQuantData.setVerticalAlignment(Element.ALIGN_MIDDLE);
 					table.addCell(pickQuantData);
 					
-					PdfPCell uomData = new PdfPCell(new Paragraph(data.get_meins()));
+					PdfPCell uomData = new PdfPCell(new Paragraph(data.get_meins(), FontFactory.getFont(FontFactory.TIMES_ROMAN, fntSize)));
 					uomData.setBorderColor(BaseColor.BLACK);
 					uomData.setPaddingLeft(10);
 					uomData.setHorizontalAlignment(Element.ALIGN_CENTER);
